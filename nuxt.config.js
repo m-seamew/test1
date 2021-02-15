871078303a0ea2c433b470894ea8dd5709190d7d
Max Brovchenko <brovchenkomaksym@gmail.com>
2021-02-15 23:22:17 +0200

    fix font

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
     title: 'blog',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1'},
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
